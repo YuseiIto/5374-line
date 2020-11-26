f70fe8f50893781696f32d62184d057e3571ec51
Yusei Ito <me@yuseiito.com>
2020-11-26 19:44:03 +0900

    友達追加メッセージに型をつける

--- src/messages/followMessage.ts
+++ src/messages/followMessage.ts
@@ -1,5 +1,7 @@
 // 友達登録時のメッセージ
+import * as line from '@line/bot-sdk';
+
 export default {
  "type": "text",
  "text": "お友達追加追加ありがとうございます🙌 \n5374 .jp @LINE for Rittoは、https://ritto.5374.jp で提供しているデータをベースに、滋賀県栗東市のゴミの日情報🗓をLINEで通知するサービスです!🔔\n\nはじめにメニューの「通知の設定」から通知を設定しましょう💨 \n ※メニューはPC/Mac版LINEではご利用いただけません🙇‍♂️"
-}
+} as line.TextMessage
